c6ebe38442bcdf5f59911a3787f8d6a5987cf674
Thomas Rix <trix@dr.dk>
2020-01-21 11:29:46 +0100

    Now basic asset handling actually works

--- deploy.js
+++ deploy.js
@@ -1,4 +1,7 @@
 const Parcel = require('parcel-bundler');
+const fs = require('fs-extra');
+const watcher = require('glob-watcher');
+
 let dev = ()=>{
     const parcel = new Parcel(
         ['./src/script/index.html', '../assets/**/*'],
@@ -6,7 +9,17 @@ let dev = ()=>{
             outDir:'./dev'
         }
         );
-        
+        fs.copy('./src/assets', './dev/assets', {overwrite:true});
+    
+        const watch = watcher('./src/assets/**/*.*', (watchdone)=>{
+            // console.log('watch triggered')
+            fs.emptyDirSync('./dev/assets');
+            fs.copy('./src/assets', './dev/assets', {overwrite:true});
+            parcel.hmr.broadcast({
+                type:'reload'
+            })
+            watchdone()
+        });
         parcel.bundle();
         parcel.serve();
     }
--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,5 @@
+import {create, select} from '../utils/trix';
 console.log('Yo!');
+
+let img = create('img', select('body'))
+img.src = '../assets/images/train.svg';
